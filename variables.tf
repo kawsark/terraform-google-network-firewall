e5ad789e194418974de56ca5a32d0762cfda72f4
kawsark <kawsar@hashicorp.com>
2020-04-13 23:26:49 -0400

    added some default variables

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "name" {
 
 variable "description" {
   description = "Description for the rule"
-  default     = ""
+  default     = "Firewall rule provisioned by terraform"
 }
 
 variable "priority" {
@@ -14,15 +14,18 @@ variable "priority" {
 
 variable "network" {
   description = "The name of the network to attach this firewall to"
+  default = "default"
 }
 
 variable "protocol" {
   description = "The name of the protocol to allow"
+  default = "tcp"
 }
 
 variable "ports" {
   description = "List of ports and/or port ranges to allow. This can only be specified if the protocol is TCP or UDP"
   type        = list(string)
+  default     = ["22"]
 }
 
 variable "source_ranges" {
